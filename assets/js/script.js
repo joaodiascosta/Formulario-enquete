de43d2b2fc0963f168d8afba57f6f9240b632d3a
joaodiascosta <joaodiasdncosta@gmail.com>
2024-03-05 16:06:40 -0300

    Verificação de campos select regiões/bairros para que não seja enviado vazio, correção de mensagem de erro dupla

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,14 +24,15 @@ function scrollToTop() {
     scroll.scrollIntoView({behavior:'smooth'});
 }
 
-var bairro = document.querySelectorAll('.distrito');
-console.log(bairro.value);
 const nomeCompleto = document.querySelector('#nome');
 const botaoEnviar = document.getElementById('button');
 const classErro = document.getElementById('nomeCompleto');
 const erroNome = document.getElementById('erro-nome');
 var erro = document.getElementById('erro');
 var outroVazio = document.getElementById('erro-vazio');
+var regiaoVazio = document.getElementById('erro-regiao');
+var bairro = document.querySelectorAll('.bairro');
+var bairrovazio = document.getElementById('erro-bairro');
 
 function verificar(frm) {
     var erros = 0;
@@ -48,28 +49,37 @@ function verificar(frm) {
         erro.style.display = 'block';
         scrollToTop();
         return false;
+    } else if (checked.length >= 1) {
+        erro.style.display = 'none'
     }
-
+    
     if(inputOutro.checked && outro.value == '') {
         erros++;
         outroVazio.style.display = 'block';
         scrollToTop();
-        // return false;
     }
-    // if(bairro.value == undefined) {
-    //     erros++;
-    // }
 
+    if(selector.value == 'Selecione a região') {
+        erros++;
+        regiaoVazio.style.display = 'block';
+    } 
+    
+    
+    if(selector.value == 'Centro' || selector.value == 'Zona Norte' || selector.value == 'Zona Sul' || selector.value == 'Zona Oeste' || selector.value == 'Zona Leste') {
+        if(centro.value == 'Selecione o bairro') {
+            erros++;
+            bairrovazio.style.display = 'block';
+        } 
+    }
     
     if(erros>0) {
         return false;
     } else  {
         toggleModal();
     }
-
+    
     frm.submit();
 }
-
 
 
 //Não autorizar números no nome
@@ -88,44 +98,37 @@ var leste = document.getElementById('leste');
 var norte = document.getElementById('norte');
 var oeste = document.getElementById('oeste');
 var sul = document.getElementById('sul');
-var bairro = document.getElementsByName('bairro');
+
 
 selector.addEventListener("click", () => {
     selector.addEventListener("change", () => {
         var valorSelect = selector.value;
         if (valorSelect == 'Centro') {
             centro.style.display = "block";
-            botaoEnviar.removeAttribute('disabled');
         } else {
             centro.style.display = "none";
         }
 
         if (valorSelect == 'Zona Leste') {
             leste.style.display = "block";
-            botaoEnviar.removeAttribute('disabled');
-
         } else {
             leste.style.display = "none";
         }
 
         if (valorSelect == 'Zona Norte') {
             norte.style.display = "block";
-            botaoEnviar.removeAttribute('disabled');
         } else {
             norte.style.display = "none";
         }
 
         if (valorSelect == 'Zona Oeste') {
             oeste.style.display = "block";
-            botaoEnviar.removeAttribute('disabled');
-
         } else {
             oeste.style.display = "none";
         }
 
         if (valorSelect == 'Zona Sul') {
             sul.style.display = "block";
-            botaoEnviar.removeAttribute('disabled');
         } else {
             sul.style.display = "none";
         }
