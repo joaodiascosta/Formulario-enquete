17c25f045ebf8630615debb79d2f7c274390c248
joaodiascosta <joaodiasdncosta@gmail.com>
2024-03-13 17:54:56 -0300

    Execução de todas as etapas sem sair do index, formulario não deixa ser enviado com o token igual

--- index.php
+++ index.php
@@ -1,9 +1,53 @@
 <?php 
     session_start();
 
-    $token = rand();
 
-    $_SESSION['token_sessao'] = $token;
+
+    // print_r($_SESSION);
+    // echo $_POST['token'];
+    // print_r($_POST);
+
+    if (isset($_POST['token'])) {
+      if ($_POST['token'] != $_SESSION['token']) {
+        echo "Formulário já enviado anteriormente.";
+        return;
+      }
+    }
+    
+    if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['token'])) {
+        include_once('config.php');
+
+        $respostaDistrito = $_POST['distrito'];
+        $respostaBairro = $_POST['bairro'];
+
+        $token = uniqid();
+        $_SESSION['token'] = $token;
+        
+        if(isset($_POST['submit'])) {
+            $resposta_1 = $_POST['pergunta1'];
+            $resposta_2 = $_POST['pergunta2'];
+            $resposta_3 = $_POST['pergunta3'];
+            $resposta_4 = implode(', ', $_POST['pergunta4']);
+            $resposta_5 = $_POST['pergunta5'];
+            $resposta_6 = $_POST['pergunta6'];
+            $resposta_7 = $_POST['pergunta7'];
+            $resposta_8 = $_POST['pergunta8'];
+            $resposta_9 = $_POST['pergunta9'];
+            $resposta_10 = $_POST['pergunta10'];
+            $participante_nome = $_POST['nome'];
+            $participante_email = $_POST['email'];
+
+            $result =  mysqli_query($conexao, "INSERT INTO respostas_participante(resposta_1, resposta_2, resposta_3, resposta_4, resposta_5, resposta_6, resposta_7, resposta_8, resposta_9, resposta_10, participante_nome, participante_email, participante_distrito) 
+            VALUES ('$resposta_1', '$resposta_2', '$resposta_3', '$resposta_4', '$resposta_5', '$resposta_6', '$resposta_7', '$resposta_8', '$resposta_9', '$resposta_10', '$participante_nome', '$participante_email', '$respostaDistrito, $respostaBairro')");
+
+            include_once('pagesucesso.php');
+            exit;
+        }
+
+        // unset($_SESSION['token_sessao']);
+    } else {
+        // include_once('pageerror.php');
+    }
     
 ?>
 <!DOCTYPE html>
@@ -21,8 +65,12 @@
         <header class="box_title">
             <h2 class="title">Enquete sobre o Plano Diretor Estratégico <br> da Cidade de São Paulo</h2>
         </header>
-        <form action="comprovante.php" method="POST" class="form" id="form" onsubmit="verificar(this); return false;">
-        <input type="hidden" name="token" value="<?php echo $token; ?>">   
+        <form method="POST" class="form" id="form" onsubmit="verificar(this); return false;">
+            <?php
+            $token = uniqid();
+            $_SESSION['token'] = $token;
+            ?>
+            <input type="hidden" name="token" value="<?php echo $token; ?>" />
         <div class="box_form">
                 <div class="pergunta primeira">
                     <p>Você sabe o que é o Plano Diretor Estratégico?</p>
